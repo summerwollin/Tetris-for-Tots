8c7fe384860e2d6be2bb7f8f3ae9db0ae345eb42
Summer Wollin <summer.wollin@gmail.com>
2016-05-20 10:04:47 -0600

    basic stripe test setup

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,29 @@
-// Update with your config settings.
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
-
-
-
-  production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    connection: 'postgres://localhost/tetrisdb',
     pool: {
       min: 2,
       max: 10
     },
-    migrations: {
-      tableName: 'knex_migrations'
+    seeds: {
+      directory: './seeds/'
     }
+  },
+
+  production: {
+  client: 'postgresql',
+  connection: process.env.DATABASE_URL,
+  pool: {
+    min: 2,
+    max: 10
+  }
+},
+
+  seeds: {
+    directory: './seeds/'
   }
 
 };
